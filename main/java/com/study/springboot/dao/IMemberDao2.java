6761675a09def2cea912de1553c205456f2ab55e
dlehdduq22 <dlehdduq22@naver.com>
2023-08-01 18:33:22 +0900

    내가만든 dao 인터페이스

---
+++ main/java/com/study/springboot/dao/IMemberDao2.java
@@ -0,0 +1,17 @@
+package com.study.springboot.dao;
+
+import java.util.List;  
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.study.springboot.dto.MemberDto2;
+
+@Mapper
+public interface IMemberDao2 {
+	public List<MemberDto2> findAll();//get 
+	public MemberDto2 findById(int id);//get
+	public int insert(MemberDto2 member); //post
+	public int update(MemberDto2 member); //put
+	public int update2(MemberDto2 member);
+	public int delete(int id);//delete
+}
