c0e6eaa4d3550a963c8d437b72298724a1b9c7c1
dlehdduq22 <dlehdduq22@naver.com>
2023-08-01 18:34:09 +0900

    내가만든 dto

---
+++ main/java/com/study/springboot/dto/MemberDto2.java
@@ -0,0 +1,11 @@
+package com.study.springboot.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto2 {
+	private int id;
+	private String name;
+	private String email;
+	private String organization; 
+}
