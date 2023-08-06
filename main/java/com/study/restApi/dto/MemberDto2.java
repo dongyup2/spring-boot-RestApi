764db5536d4b8099fa9032df605f2e68c81db0ff
dlehdduq22 <dlehdduq22@naver.com>
2023-08-06 23:40:20 +0900

    연습

---
+++ main/java/com/study/restApi/dto/MemberDto2.java
@@ -0,0 +1,11 @@
+package com.study.restApi.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto2 {
+	private int id;
+	private String name;
+	private String email;
+	private String organization; 
+}
