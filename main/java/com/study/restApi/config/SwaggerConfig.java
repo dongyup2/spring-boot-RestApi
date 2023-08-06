942406935af43c7f97d0d00e39c909ba128c9701
dlehdduq22 <dlehdduq22@naver.com>
2023-08-06 23:41:46 +0900

    no message

---
+++ main/java/com/study/restApi/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.study.restApi.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+	public GroupedOpenApi bublicApi() {
+		return GroupedOpenApi.builder()
+				.group("v1-definition")
+				.pathsToMatch("/api/**")
+				.build();
+	}
+	
+	@Bean
+	public OpenAPI springShopOpenApi() {
+		return new OpenAPI()
+				.info(new Info().title("REST API TEST")
+				.description("RestAPI 명세서입니다.")
+				.version("V0.0.1"));
+	}
+}
